dfb0d08fcf715a078c5a21fc8467b56a961bce34
D-DeathUser-D <ilkolreserv@gmail.com>
2024-04-07 16:53:57 +0400

    hasid

--- src/antlr/AST/Declaration.ts
+++ src/antlr/AST/Declaration.ts
@@ -1,28 +1,6 @@
-import { Range } from "vscode";
-import { ASTNode } from "./ASTNode";
+import { HasID } from "./HasID";
 
-export abstract class Declaration extends ASTNode
+export abstract class Declaration extends HasID
 {
-	private _identifire: string = "";
-	private _idPos: Range = new Range(0,0,0,0);
-	public constructor()
-	{
-		super();
-	}
-	
-	public get id() : string {
-		return this._identifire;
-	}
-	
-	public set id(v : string) {
-		this._identifire = v;
-	}
-
-	public get idPos() : Range {
-		return this._idPos;
-	}
-	
-	public setIDPos(line:number, start: number, end: number) {
-		this._idPos = new Range(line - 1, start, line - 1, end);
-	}	
+		
 }
---
+++ src/antlr/AST/HasID.ts
@@ -0,0 +1,33 @@
+import { Range } from "vscode";
+import { ASTNode } from "./ASTNode";
+
+export abstract class HasID extends ASTNode
+{
+	private _identifire: string = "";
+	private _idPos: Range = new Range(0,0,0,0);
+	public constructor()
+	{
+		super();
+	}
+	
+	public get id() : string {
+		return this._identifire;
+	}
+	
+	public set id(v : string) {
+		this._identifire = v;
+	}
+
+	public get idPos() : Range {
+		return this._idPos;
+	}
+	
+	public setIDPos(pos: Range): void
+	public setIDPos(line: number, start: number, end: number): void
+	public setIDPos(line: Range | number, start?: number, end?: number): void {
+		if(typeof line == "number")
+			this._idPos = new Range(line - 1, <number>start, line - 1, <number>end);
+		else 
+			this._idPos = line;
+	}
+}
