017e8671182c0900f1f75b69d4df7e3da456ef9f
D-DeathUser-D <ilkolreserv@gmail.com>
2024-04-11 22:56:14 +0400

    добавил поддержку hex

--- src/Providers/ColorProvider.ts
+++ src/Providers/ColorProvider.ts
@@ -6,8 +6,8 @@ export class PawnColorProvider implements DocumentColorProvider
 		let colors: ColorInformation[] = [];
 		
 		let text = document.getText();     
-		let reg = /{([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})}/gi;
 		let match, range: Range, pos: Position;
+		let reg = /{([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})}/gi;
 		while((match = reg.exec(text))) {
 			pos = document.positionAt(match.index);
 			range = new Range(pos.line, pos.character, pos.line, pos.character + match[0].length);
@@ -15,24 +15,45 @@ export class PawnColorProvider implements DocumentColorProvider
 			colors.push(new ColorInformation(range, color));
 			
 		}
+		reg = /0x([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/gi;
+		while((match = reg.exec(text))) {
+			pos = document.positionAt(match.index);
+			range = new Range(pos.line, pos.character, pos.line, pos.character + match[0].length);
+			let color = new Color(parseInt(match[1], 16) / 255, parseInt(match[2], 16) / 255, parseInt(match[3], 16) / 255, parseInt(match[4], 16) / 255);
+			colors.push(new ColorInformation(range, color));
+			
+		}
 		return colors;
 	}
 	provideColorPresentations(color: Color, context: { readonly document: TextDocument; readonly range: Range; }, token: CancellationToken): ProviderResult<ColorPresentation[]> {
-		let hex = this.rgbToHex(color);
-		if(/^[a-f\d]{6}/i.test(hex)) {
-			let val = new ColorPresentation(hex)
-			val.textEdit = new TextEdit(context.range, "{" + hex + "}");
-			return [val];
+		let text = context.document.getText(context.range);
+		if(text[0] == "{") {
+			let hex = this.rgbToHex(color);
+			if(/^[a-f\d]{6}/i.test(hex)) {
+				let val = new ColorPresentation(hex)
+				val.textEdit = new TextEdit(context.range, "{" + hex + "}");
+				return [val];
+			}      
+		}
+		else {
+			let hex = this.rgbToHex(color, color.alpha);
+			if(/^[a-f\d]{8}/i.test(hex)) {
+				let val = new ColorPresentation(hex)
+				val.textEdit = new TextEdit(context.range, "0x" + hex);
+				return [val];
 
-		}      
-		else return [];
+			}      
+		}
+		return [];
 	}
 	private componentToHex(color: number): string {
 		return color.toString(16).padStart(2, '0');
 	}
-	private rgbToHex(color: Color) {
-		let red = Math.round(color.red * 255);
-		return this.componentToHex(red) + this.componentToHex(Math.round(color.green * 255)) +  this.componentToHex(Math.round(color.blue * 255));
+	private rgbToHex(color: Color, alpha: number | undefined = undefined) {
+		return this.componentToHex(Math.round(color.red * 255)) 
+		+ this.componentToHex(Math.round(color.green * 255)) 
+		+  this.componentToHex(Math.round(color.blue * 255)) 
+		+ (alpha !== undefined ? this.componentToHex(Math.round(alpha * 255)) : "");
 	}
 	  
 }
