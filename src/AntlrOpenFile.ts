e24ca2b37bc1aada9cf5f6da9a4b1379f29141ac
D-DeathUser-D <ilkolreserv@gmail.com>
2024-06-20 17:39:23 +0400

    delete extra code

--- src/AntlrOpenFile.ts
+++ src/AntlrOpenFile.ts
@@ -60,7 +60,6 @@ export class AntrlOpenFile extends AbstractOpenFile
 	private diagnostic(diagnostics: DiagnosticMessage[])
 	{
 		diagnostics.forEach(element => {
-			console.log(element.tags);
 			this.diagnositcManager.addDiagnostic(element.message, element.type, this.file.uri.path, element.pos, element.tags);
 		});
 	}
