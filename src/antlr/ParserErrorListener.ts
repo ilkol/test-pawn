e6e7452526f3c73e7c72efd820d1a2f8b362dedf
ilkol <160573175+ilkol@users.noreply.github.com>
2024-07-03 13:52:05 +0400

    сраные ошибки

--- src/antlr/ParserErrorListener.ts
+++ src/antlr/ParserErrorListener.ts
@@ -1,6 +1,6 @@
 import { ANTLRErrorListener, CommonToken, RecognitionException, Recognizer, Token } from "antlr4ts";
 import * as antlr from "antlr4ts";
-import { FunctionDeclContext } from "./generated/pawnParser";
+import { FunctionDeclContext, pawnParser } from "./generated/pawnParser";
 import { DiagnosticMessage } from "./diagnostic/DiagnosticMessage";
 import { DiagnosticError } from "./diagnostic/DiagnosticError";
 import { Position, Range } from "vscode";
@@ -9,14 +9,20 @@ export class ParserErrorListener implements ANTLRErrorListener<Token>
 {
 	private _diagnostic: DiagnosticMessage[] = [];
 	syntaxError <T extends Token>(recognizer: Recognizer<T, any>, offendingSymbol: T | undefined, line: number, charPositionInLine: number, msg: string, e: RecognitionException | undefined): void {
-		if(e) {
-			if(e.context instanceof FunctionDeclContext) {
-				if(offendingSymbol?.text == "<EOF>") {
-					this.diagnostic.push(new DiagnosticError(`Ожидается ";"`, this.getPos(line, charPositionInLine, charPositionInLine+1)));
-					// console.log("Ожидается ;");
-				}
-			}
-		}
+		// console.error("PARSER ERROR");
+		// if(msg.includes("missing ';")) {
+		// 	recognizer
+		this.diagnostic.push(new DiagnosticError(msg, this.getPos(line - 1, charPositionInLine, charPositionInLine+1)));
+		// }
+		// if(e) {
+		// 	console.error(e.expectedTokens);
+		// 	if(e.context instanceof FunctionDeclContext) {
+		// 		if(offendingSymbol?.text == "<EOF>") {
+		// 			this.diagnostic.push(new DiagnosticError(`Ожидается ";"`, this.getPos(line, charPositionInLine, charPositionInLine+1)));
+		// 			// console.log("Ожидается ;");
+		// 		}
+		// 	}
+		// }
 		
 	}
 
