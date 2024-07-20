ffbf787ff544c0cf4d8f88d08e89286830d97fb0
ilkol <160573175+ilkol@users.noreply.github.com>
2024-07-20 12:12:22 +0400

    удалил лишний вывод

--- src/extension.ts
+++ src/extension.ts
@@ -82,7 +82,6 @@ export async function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.onDidSaveTextDocument((file) => {
 		if(file.languageId != "pawn") return;
 		console.error("SAVE FILE");
-		console.error(fileManage.openedFiles);
 		return fileManage.onDidChangeDocument(file);
 	});
 
