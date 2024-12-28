173ad254087a1f986c4c828747a29427c3d69492
ilkol <160573175+ilkol@users.noreply.github.com>
2024-12-28 23:09:33 +0400

    терзают мутные сомнения

--- src/Prepocessor/Define.ts
+++ src/Prepocessor/Define.ts
@@ -40,6 +40,7 @@ export class Define extends PreprocessorDirective
 	}
 	private preparePattern(rest: string): RestData
 	{
+		console.log("define", rest);
 		const reg = /(\s*)([^\s]+)(?:\s+(.+))?/;
 		const match = reg.exec(rest);
 
