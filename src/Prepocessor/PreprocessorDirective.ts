8950f06515ade0c6cc63c59aa0d9b950db0740ed
ilkol <ilkolreserv@gmail.com>
2025-02-22 21:23:32 +0400

    delete skiped

--- src/Prepocessor/PreprocessorDirective.ts
+++ src/Prepocessor/PreprocessorDirective.ts
@@ -2,7 +2,6 @@ import { Position, Range, TextDocument } from "vscode";
 
 export abstract class PreprocessorDirective
 {
-	public skiped: boolean = false;
 	readonly range: Range;
 	public curStartIndex: number;
 	public curEndIndex: number;
