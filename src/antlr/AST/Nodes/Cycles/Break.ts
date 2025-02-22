5126b5eaeb6fa80f00a18cd6f89419b8b65ef0cf
ilkol <ilkolreserv@gmail.com>
2025-02-22 21:23:16 +0400

    временные accpet

--- src/antlr/AST/Nodes/Cycles/Break.ts
+++ src/antlr/AST/Nodes/Cycles/Break.ts
@@ -1,5 +1,10 @@
+import { IVisitor } from "../../visitor/IVisitor";
 import { Statement } from "../Statement";
 
 export class Break extends Statement {
 	name = "Break";
+
+	public accept(visitor: IVisitor): void {
+		
+	}
 }
--- src/antlr/AST/Nodes/Cycles/Continue.ts
+++ src/antlr/AST/Nodes/Cycles/Continue.ts
@@ -1,5 +1,10 @@
+import { IVisitor } from "../../visitor/IVisitor";
 import { Statement } from "../Statement";
 
 export class Continue extends Statement {
 	name = "Continue";
+
+	public accept(visitor: IVisitor): void {
+		
+	}
 }
