13d53af17f15d29e0a5f23fafd561763a1667af5
ilkol <ilkolreserv@gmail.com>
2025-02-27 22:20:20 +0400

    extra import

--- src/antlr/PawnListener.ts
+++ src/antlr/PawnListener.ts
@@ -8,7 +8,6 @@ import { pawnListener } from "./generated/pawnListener";
 import { ArrayIndexContext, ArrayIndexOperatorContext, ArrayInitContext, ArrayOperatorCharContext, ArrayOperatorIndexContext, BinarExpressionOperatorContext, BinaryContext, Bool_constContext, CaseContext, ChainedRelationalOperatorContext, ChainedRelationalOperatorsContext, CompoundStatmentContext, CycleKeywordsContext, DeclParamsContext, DefaultContext, DocBlockContext, EllipseContext, ElseStatementContext, EnumContext, EnumMemberContext, ExpresionContext, FileContext, FloatContext, ForContext, FuncDeclModifContext, FunctionCallOperatorContext, FunctionDeclContext, HexContext, IfStatementContext, IntegerContext, NativeAssigmentContext, OperatorOverloadContext, PostDecrementContext, PostIncrementContext, PreDecrementContext, PreExpresionOperatorContext, PreIncrementContext, PreSymbolOperatorContext, PredefinedConstantsContext, RationalContext, ReturnContext, StatementContext, StringContext, SwitchContext, SymbolContext, TagContext, TagOperatorContext, TagableExpressionContext, UnarOperatorContext, VarDeclarationContext, VarInitContext, VarModifiresContext, VariableContext, WhileContext } from "./generated/pawnParser";
 import { VarDeclaration } from "./AST/Nodes/Variables/VarDeclaration";
 import { OperatorNew, VariableModifire } from "./AST/Nodes/Operators/OperatorNew";
-import { TerminalNode } from "antlr4ts/tree/TerminalNode";
 import { EnumDeclaration } from "./AST/Nodes/enum/EnumDeclaration";
 import { EnumMember } from "./AST/Nodes/enum/EnumMember";
 import { Tag } from "./AST/Nodes/Tag";
