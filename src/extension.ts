73a60c73989e0fd4da57f44b122cd74a65b70456
ilkol <ilkolreserv@gmail.com>
2025-08-21 22:39:11 +0400

    Reorganize extension initializing

--- src/extension.ts
+++ src/extension.ts
@@ -14,31 +14,102 @@ import { getDefaultCompletions } from './DefaultCompletions/DefaultCompletions';
 import { serializeInit } from './cache/Serialization/serializeInit';
 import { CacheManager } from './cache/CacheManager';
 
-let diagnosticManager: DiagnosticManager;
-let fileManage: FileManager;
+class Extension {
+
+	/**
+	 * Версия расширения
+	 */
+	public readonly version: string;
+	
+	/**
+	 * Класс отвечающий за управление диагностическими сообщениями 
+	 * (предупреждения, ошибки, советы)
+	 */
+	private _diagnosticManager: DiagnosticManager;
+	/**
+	 * Класс отвечающий за управление файлами
+	 */
+	private _fileManager: FileManager;
+
+	/**
+	 * Провайдер для ссылок
+	 */
+	public readonly referenceProvider: ReferenceProvider;
+	/**
+	 * Провайдер для определений
+	 */
+	public readonly definitionProvider: DefinitionProvider;
+
+	constructor(context: vscode.ExtensionContext) {
+		this.version = context.extension.packageJSON.version;
+		
+		this._diagnosticManager = new DiagnosticManager(vscode.languages.createDiagnosticCollection("pawn"));
+		this.definitionProvider = new DefinitionProvider();
+		this.referenceProvider = new ReferenceProvider();
+		this._fileManager = new FileManager(this._diagnosticManager, this.definitionProvider, this.referenceProvider);
+	}
+
+	get diagnosticManager(): DiagnosticManager {
+		return this._diagnosticManager;
+	}
+	get fileManager(): FileManager {
+		return this._fileManager;
+	}
+}
+
 
 export async function activate(context: vscode.ExtensionContext) {
-	const version = context.extension.packageJSON.version;
-	console.debug(`Активация TEST расширения v${version}!`);
+	const extension = new Extension(context);
+	console.debug(`Активация TEST расширения v${extension.version}!`);
 
-	diagnosticManager = new DiagnosticManager(vscode.languages.createDiagnosticCollection("pawn"));
-	const definitionProvider = new DefinitionProvider();
-	const referenceProvider = new ReferenceProvider();
-	fileManage = new FileManager(diagnosticManager, definitionProvider, referenceProvider);
 
-	await fileManage.findPawnDir();
-	const documentLinkProvider = new DocumentLinkProvider(fileManage);
+	await extension.fileManager.findPawnDir();
 
 	serializeInit();
 	CacheManager.initialize(context);
-	if(version !== CacheManager.getExtensionVersion()) {
+	if(extension.version !== CacheManager.getExtensionVersion()) {
 		CacheManager.flushWorkspaceCache();
-		console.debug(`Сброс кэша расширения из-за обновления версии с ${CacheManager.getExtensionVersion()} на ${version}`);
-		CacheManager.setExtensionVersion(version);
+		CacheManager.setExtensionVersion(extension.version);
 		vscode.window.showInformationMessage('Кэш рабочей области был очищен из-за обновления расширения. Пожалуйста, перезапустите VSCode для полной работы.');
 	}
 
+	registerEventListeners(context, extension);
+	registerProviders(context, extension);
+	registerTasks(context);
+	registerCommands(context, extension);
 
+	parseAllOpenedFiles(extension);
+}
+
+export async function deactivate() {
+	// TODO: сохранение кэша
+}
+
+function registerEventListeners(context: vscode.ExtensionContext, extension: Extension) {
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeTextDocument(async (e) => {
+			if (e.document.languageId !== "pawn") { return; }
+			let connect = e.contentChanges;
+			if (!connect.length) {
+				return;
+			}
+			if (connect[0].text === ";") {
+				await extension.fileManager.onDidChangeDocument(e.document);
+			}
+		}),
+		vscode.workspace.onDidOpenTextDocument(
+			(doc) => extension.fileManager.onDidOpenTextDocument(doc),
+			extension.fileManager
+		),
+		vscode.workspace.onDidSaveTextDocument(async (file) => {
+			if (file.languageId !== "pawn") { return; }
+			console.error("SAVE FILE");
+			return await extension.fileManager.onDidChangeDocument(file);
+		}),
+	);
+}
+
+function registerProviders(context: vscode.ExtensionContext, extension: Extension) {
 	const tokenTypes = [
 		SemanticTokens.type,
 		SemanticTokens.enum,
@@ -64,83 +135,36 @@ export async function activate(context: vscode.ExtensionContext) {
 	];
 	const legend = new vscode.SemanticTokensLegend(tokenTypes, tokenModifiers);
 
-	const documentSemanticTokensProvider = new DocumentSemanticTokensProvider(fileManage, legend);
+	context.subscriptions.push(
+		vscode.languages.registerDocumentLinkProvider('pawn', new DocumentLinkProvider(extension.fileManager)),
+		vscode.languages.registerReferenceProvider('pawn', extension.referenceProvider),
+		vscode.languages.registerDefinitionProvider('pawn', extension.definitionProvider),
+		vscode.languages.registerDocumentSymbolProvider('pawn', new SymbolProvider(extension.fileManager)),
+		vscode.languages.registerColorProvider('pawn', new PawnColorProvider()),
+		vscode.languages.registerDocumentSemanticTokensProvider('pawn', new DocumentSemanticTokensProvider(extension.fileManager, legend), legend),
+		vscode.languages.registerSignatureHelpProvider('pawn', new SignatureProvider(extension.fileManager), { triggerCharacters: ['(', ','], retriggerCharacters: [")"] }),
+		vscode.languages.registerHoverProvider('pawn', {
+			async provideHover(document, position, token) {
+				return extension.fileManager.registerHover(document, position);
+			}
+		}),
+		vscode.languages.registerCompletionItemProvider('pawn', {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
+				let completions: vscode.CompletionItem[] = getDefaultCompletions();
 
-	vscode.workspace.onDidChangeTextDocument(async (e) => {
-		if (e.document.languageId !== "pawn") { return; }
-		let connect = e.contentChanges;
-		if (!connect.length) {
-			return;
-		}
-		if (connect[0].text === ";") {
-			await fileManage.onDidChangeDocument(e.document);
-		}
-	});
-	vscode.workspace.onDidOpenTextDocument(fileManage.onDidOpenTextDocument, fileManage);
-	parseAllOpenedFiles();
-
-
-	vscode.workspace.onDidSaveTextDocument(async (file) => {
-		if (file.languageId !== "pawn") { return; }
-		console.error("SAVE FILE");
-		return await fileManage.onDidChangeDocument(file);
-	});
-
-
-	//регистрируем таск
-	registerTasks(context);
-	registerCommands(context);
-
-	
-
-	const signatureProvider = new SignatureProvider(fileManage);
-	context.subscriptions.push(vscode.languages.registerDocumentLinkProvider('pawn', documentLinkProvider));
-
-	context.subscriptions.push(vscode.languages.registerReferenceProvider('pawn', referenceProvider));
-	context.subscriptions.push(vscode.languages.registerDefinitionProvider('pawn', definitionProvider));
-
-	const symbolProvider = new SymbolProvider(fileManage);
-	context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider('pawn', symbolProvider));
-
-	//пока бесполезно
-	// const callHierarchyProvider = new CallHierarchyProvider();
-	// context.subscriptions.push(vscode.languages.registerCallHierarchyProvider('pawn', callHierarchyProvider));
-	//пока бесполезно
-
-	const colorProvider = new PawnColorProvider();
-	context.subscriptions.push(vscode.languages.registerColorProvider('pawn', colorProvider));
-	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider('pawn', documentSemanticTokensProvider, legend));
-	context.subscriptions.push(vscode.languages.registerSignatureHelpProvider('pawn', signatureProvider, { triggerCharacters: ['(', ','], retriggerCharacters: [")"] }));
-
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('pawn', {
-		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-			const edits: vscode.TextEdit[] = [];
-			return edits;
-		}
-	}));
-
-	context.subscriptions.push(vscode.languages.registerHoverProvider('pawn', {
-		async provideHover(document, position, token) {
-			return fileManage.registerHover(document, position);
-		}
-	}));
-
-	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('pawn', {
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-			let completions: vscode.CompletionItem[] = getDefaultCompletions();
-
-			const file: AbstractOpenFile | undefined = fileManage.openedFiles.get(document.uri);
-			if (file) { completions = completions.concat(file.getCompletions()); }
-			return completions;
-		}
-	})
+				const file: AbstractOpenFile | undefined = extension.fileManager.openedFiles.get(document.uri);
+				if (file) { completions = completions.concat(file.getCompletions()); }
+				return completions;
+			}
+		}),
+		vscode.languages.registerDocumentFormattingEditProvider('pawn', {
+			provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+				const edits: vscode.TextEdit[] = [];
+				return edits;
+			}
+		}),
 	);
-}
-
-export async function deactivate() {
-	// TODO: сохранение кэша
 }
 
 function registerTasks(context: vscode.ExtensionContext) {
@@ -164,36 +188,46 @@ function registerTasks(context: vscode.ExtensionContext) {
 	);
 }
 
-function parseAllOpenedFiles() {
-	vscode.workspace.textDocuments.forEach(document => {
-		fileManage.onDidOpenTextDocument(document);
+function parseAllOpenedFiles(extension: Extension) {
+	vscode.workspace.textDocuments.forEach(async (document) => {
+		if (document.languageId !== 'pawn') { 
+			return; 
+		}
+		try {
+			await extension.fileManager.onDidOpenTextDocument(document);
+		} catch (error) {
+			vscode.window.showErrorMessage(`Ошибка при парсинге файла ${document.fileName}`);
+			console.error(error);
+		}
 	});
 }
-function registerCommands(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand("pawnlanguage.openParsed",() => {
-		if(!vscode.window.activeTextEditor) {
-			return;
-		}
-		const file = fileManage.getFileByURI(vscode.window.activeTextEditor.document.uri);
-		file?.openFileWithoutPreprocessor();
-	}));
-	context.subscriptions.push(vscode.commands.registerCommand("pawnlanguage.parseCurrnetFile",() => {
-		if(!vscode.window.activeTextEditor) {
-			return;
-		}
-		fileManage.onDidOpenTextDocument(vscode.window.activeTextEditor.document);
+function registerCommands(context: vscode.ExtensionContext, extension: Extension) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand("pawnlanguage.openParsed",() => {
+			if(!vscode.window.activeTextEditor) {
+				return;
+			}
+			const file = extension.fileManager.getFileByURI(vscode.window.activeTextEditor.document.uri);
+			file?.openFileWithoutPreprocessor();
+		}),
+		vscode.commands.registerCommand("pawnlanguage.parseCurrnetFile",() => {
+			if(!vscode.window.activeTextEditor) {
+				return;
+			}
+			extension.fileManager.onDidOpenTextDocument(vscode.window.activeTextEditor.document);
+			
+		}),
+		vscode.commands.registerCommand("pawnlanguage.flushFileCache",() => {
+			if (!vscode.window.activeTextEditor) {
+				return;
+			}
+			CacheManager.flushFileCache(vscode.window.activeTextEditor.document.uri.path);
+			vscode.window.showInformationMessage('Кэш файла очищен');
 		
-	}));
-	context.subscriptions.push(vscode.commands.registerCommand("pawnlanguage.flushFileCache",() => {
-		if (!vscode.window.activeTextEditor) {
-			return;
-		}
-		CacheManager.flushFileCache(vscode.window.activeTextEditor.document.uri.path);
-		vscode.window.showInformationMessage('Кэш файла очищен');
-	
-	}));
-	context.subscriptions.push(vscode.commands.registerCommand("pawnlanguage.flushWorkspaceCache",() => {
-		CacheManager.flushWorkspaceCache();
-		vscode.window.showInformationMessage('Кэш рабочей области очищен');
-	}));
+		}),
+		vscode.commands.registerCommand("pawnlanguage.flushWorkspaceCache",() => {
+			CacheManager.flushWorkspaceCache();
+			vscode.window.showInformationMessage('Кэш рабочей области очищен');
+		})
+	);
 }
