1fbc48b138e501c1d939dff086316c32f09fe671
ilkol <ilkolreserv@gmail.com>
2026-02-25 11:57:02 +0400

    add check tagmismatch on binary

--- server/src/antlr/AST/visitor/Analyzer.ts
+++ server/src/antlr/AST/visitor/Analyzer.ts
@@ -421,6 +421,13 @@ export class Analyzer extends BaseVisitor
 		
 		node.setRange(node.left.pos.start, node.right.pos.end);
 		node.inferredTag = this.tagInferer.inferTag(node);
+
+		const leftTag = node.left.inferredTag;
+		const rightTag = node.right.inferredTag;
+
+		if (!leftTag || !rightTag || !this.tagInferer.findUserOperator(node.operator, leftTag, rightTag, 2)) {
+			this.checkTagMismatch(leftTag, rightTag, false, node.range);
+   		 }
 		
 	}
 	beforeVisitReturn(node: ReturnStatement): void {
